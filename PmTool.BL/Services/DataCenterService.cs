f5a563339f58a36c3b2f2b55cb1e3ced86fdb0e8
Cristhiam <cristhiammena91@hotmail.com>
2019-02-04 18:02:49 -0600

    Se agrega un metodo

--- PmTool.BL/Services/DataCenterService.cs
+++ PmTool.BL/Services/DataCenterService.cs
@@ -20,10 +20,30 @@ namespace PmTool.BL.Services
             this.dbContext = new ProjectManagementEntities();
             this.unitOfWork = new UnitOfWork(dbContext);
         }
-
+        /// <summary>
+        /// The method get a Id from user and return the data center with the same Id.
+        /// </summary>
+        /// <param name="id">Data center id</param>
+        /// <returns>The object data center with the id</returns>
         public DataCenters GetDataCenterById(int id)
         {
             return unitOfWork.Repository<DataCenters>().GetById(id);
+        }
+
+        /// <summary>
+        /// The method get all the data centers of the data base
+        /// </summary>
+        /// <returns>A list of all the data centers</returns>
+        public List<DataCenters> listAllDataCenterProjects()
+        {
+            List<DataCenters> theListOfAllDataCenters = unitOfWork.Repository<DataCenters>().GetAll().ToList();
+            return theListOfAllDataCenters;
+        }
+
+        public List<DataCenters> listAllUserDataCenterProjects(int userId)
+        {
+            List<DataCenters> theListOfAllUserDataCenters = unitOfWork.Repository<DataCenters>().GetAll().ToList();
+
         }
 
     }
